fc1e205fc3972577f3ca8d1e4b47e444a1a10916
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2024-11-11 11:21:50 +0100

    add basic commands and maybe recuperate board size

--- src/main/java/ch/heigvd/dai/commands/Server.java
+++ src/main/java/ch/heigvd/dai/commands/Server.java
@@ -1,4 +1,36 @@
 package ch.heigvd.dai.commands;
 
-public class Server {
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.*;
+
+@CommandLine.Command(name = "server", description = "Start the server part of the network game.")
+public class Server implements Callable<Integer> {
+
+    @CommandLine.Option(
+            names = {"-p", "--port"},
+            description = "Port to use (default: ${DEFAULT-VALUE}).",
+            defaultValue = "6433")
+    protected int port;
+
+    /*
+    // Definition of the parameter for board size
+    @CommandLine.Option(
+            names = {"-s", "--size"},
+            description = "The size of the board wanted (max 9).",
+            defaultValue = "3")
+    protected int BoardSize;
+
+    public int getBoardSize() {
+        return BoardSize;
+    }
+    
+     */
+
+    @Override
+    public Integer call() {
+        throw new UnsupportedOperationException(
+                "Please remove this exception and implement this method.");
+    }
 }
