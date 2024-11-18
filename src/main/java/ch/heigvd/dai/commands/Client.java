faa4e0490490acdee84d0b732c01a5e508c68b37
Lisa Gorgerat <lisa.gorgerat@heig-vd.ch>
2024-11-18 09:46:10 +0100

    begining implementation of call

--- src/main/java/ch/heigvd/dai/commands/Client.java
+++ src/main/java/ch/heigvd/dai/commands/Client.java
@@ -1,7 +1,11 @@
 package ch.heigvd.dai.commands;
 
 import java.util.concurrent.Callable;
+import java.io. *;
+import java.net.*;
 import picocli.CommandLine;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @CommandLine.Command(name = "client", description = "Start the client part of the network game.")
 public class Client implements Callable<Integer> {
@@ -20,7 +24,11 @@ public class Client implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        throw new UnsupportedOperationException(
-                "Please remove this exception and implement this method.");
+        try (Socket socket = new Socket(host, port);) {
+            System.out.println("Client connected to " + host + ":" + port);
+        } catch (IOException e) {
+            System.out.println("Client exception: " + e);
+        }
+        return 0;
     }
 }
--- src/main/java/ch/heigvd/dai/commands/Server.java
+++ src/main/java/ch/heigvd/dai/commands/Server.java
@@ -1,9 +1,13 @@
 package ch.heigvd.dai.commands;
 
+import java.io.IOException;
 import java.util.concurrent.Callable;
 import picocli.CommandLine;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.*;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @CommandLine.Command(name = "server", description = "Start the server part of the network game.")
 public class Server implements Callable<Integer> {
@@ -29,8 +33,20 @@ public class Server implements Callable<Integer> {
      */
 
     @Override
-    public Integer call() {
-        throw new UnsupportedOperationException(
-                "Please remove this exception and implement this method.");
+    public Integer call() throws IOException {
+
+        try (ServerSocket serverSocket = new ServerSocket(port);
+            ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();) {
+            System.out.println("Server started on port " + port);
+
+            while (!serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                executor.submit(new ClientHandler(clientSocket));
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception: " + e);
+            return -1;
+        }
+        return 0; // Success
     }
 }
